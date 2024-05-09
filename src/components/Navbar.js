d3a45dda9a6df44433363cf8354e92edc4e32625
BillysugarcaneRoy <mailelroi99@gmail.com>
2024-05-09 16:47:08 +0100

    create basic react setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<>
+			<nav>
+				<div className='nav-container'>
+					<ul className='nav-list'>
+						<li></li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
+};
+
+export default Navbar;
